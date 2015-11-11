2640b132ec3807c9b6999c3644cfa2f8afab54f9
Luke Snowden <luke@purposemedia.co.uk>
2015-11-11 13:59:30 +0000

    mistake... sorry

--- instances_of_number_in_array/solution.php
+++ instances_of_number_in_array/solution.php
@@ -1,5 +1,5 @@
 <?php
 function solution($array) {
-    return max(array_count_values($array));
+    return key( array_count_values( $array ) );
 }
-var_dump(solution([1, 1, 3, 4, 5, 5, 5, 6, 7, 5, 4, 6, 4, 5]));
+var_dump(solution([1, 3, 4, 5, 5, 5, 6, 7, 5, 4, 6, 4, 5]));
